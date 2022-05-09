dd07495b565cc53d35a03cf6e0e5c2915da6b238
Kevin Hertwig <hertwig@campus.tu-berlin.de>
2022-05-09 03:15:43 +0200

    chore(index.ts): exporting everything from @whitelabel-solutions/wallet-connector package

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+export type {
+  IProvider,
+  ConnectorUserOptions,
+  ProviderType,
+} from "@whitelabel-solutions/wallet-connector";
+export { Connector, Providers } from "@whitelabel-solutions/wallet-connector";
 export * from "./components/modal";
