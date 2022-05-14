b585569f1a81be9c773cdf4396ee0903c8cf4044
Kevin Hertwig <hertwig@campus.tu-berlin.de>
2022-05-14 03:25:54 +0200

    chore(index.ts): adding more eip conformant types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,25 +1,16 @@
-import type { EventEmitter } from "node:events";
-
 // per EIP-1193
-export interface RpcParams {
+export interface RequestArguments {
   readonly method: string;
   readonly params?: readonly unknown[] | object;
 }
 
 // per EIP-1193
-export interface Provider extends EventEmitter {
-  request(args: RpcParams): Promise<unknown>;
-  connect(): void;
-  disconnect(): void;
-}
-
-// per EIP-1193
-export interface ConnectInfo {
+export interface ProviderConnectInfo {
   readonly chainId: string;
 }
 
 // per EIP-1193
-export interface RpcError extends Error {
+export interface ProviderRpcError extends Error {
   message: string;
   code: number;
   data?: unknown;
@@ -27,24 +18,66 @@ export interface RpcError extends Error {
 
 // per EIP-3085
 // Interface for adding a new Ethereum chain to the wallet
-export interface AddChainParams {
-  chainId: number;
-  chainName: string;
-  nativeCurrency: {
-    name: string;
-    symbol: string; // 2-6 characters long
-    decimals: 18;
-  };
-  rpcUrls: string[];
+export interface AddEthereumChainParameter {
+  chainId: string;
   blockExplorerUrls?: string[];
-  iconUrls?: string[]; // Currently ignored.
+  chainName?: string;
+  iconUrls?: string[];
+  nativeCurrency?: {
+    name: string;
+    symbol: string;
+    decimals: number;
+  };
+  rpcUrls?: string[];
 }
 
 // per EIP-747
 // Interface to add a new token to the wallet
-export interface WatchAssetParams {
-  address: string; // Contract address of the token
-  symbol: string; // The token symbol
-  decimals: number; // The number of decimals
-  image: string; // A string url to the logo of the token
+export interface WatchAssetParameters {
+  type: string; // The asset's interface, e.g. 'ERC20'
+  options: {
+    address: string; // The hexadecimal Ethereum address of the token contract
+    symbol?: string; // A ticker symbol or shorthand, up to 5 alphanumerical characters
+    decimals?: number; // The number of asset decimals
+    image?: string; // A string url of the token logo
+  };
+}
+
+export interface ProviderMessage {
+  readonly type: string;
+  readonly data: unknown;
+}
+
+//
+export interface EthereumEvent {
+  connect: ProviderConnectInfo;
+  disconnect: ProviderRpcError;
+  accountsChanged: string[];
+  chainChanged: number;
+  message: ProviderMessage;
+}
+
+type EventKeys = keyof EthereumEvent;
+type EventHandler<K extends EventKeys> = (event: EthereumEvent[K]) => void;
+
+export interface Ethereumish {
+  chainId: string;
+  isMetaMask?: boolean;
+  isStatus?: boolean;
+  networkVersion: string;
+  selectedAddress: string;
+
+  on<K extends EventKeys>(event: K, eventHandler: EventHandler<K>): void;
+  enable(): Promise<string[]>;
+  request(args: RequestArguments): Promise<unknown>;
+  removeAllListeners: () => void;
+
+  /**
+   * @deprecated
+   */
+  send?: (...args: unknown[]) => unknown;
+  sendAsync?: (
+    request: { method: string; params?: Array<any> },
+    callback: (error: any, response: any) => void
+  ) => void;
 }
